8af1dbe9afe98949f00829a394aa3df0cd21cf63
ADITYA SWAROOP <adityaswaroop80@gmail.com>
2023-05-16 01:10:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Frontend Developer", " Graphic Designer", "Singer", "Youtuber", "Freelancer"],
+        strings: ["Frontend Developer", " Graphic Designer", "Singer", "Youtuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Frontend Developer", " Graphic Designer", "Singer", "Youtuber", "Freelancer"]],
+        strings: ["Frontend Developer", " Graphic Designer", "Singer", "Youtuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
